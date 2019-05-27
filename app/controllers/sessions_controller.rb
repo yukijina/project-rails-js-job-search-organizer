8b7a92ec84dd541815b2d12dae7a65b8d023711e
yukijina <yukijina@gmail.com>
2019-05-26 20:06:15 -0700

    create login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,11 +12,20 @@ class SessionsController < ApplicationController
 
   #login - create
   def create
-
+    @user = User.find_by(id: params[:user][:id])
+    if @user && @user.save
+      session[:user_id] = @user.id
+    else
+      render 'login'
+    end
   end
 
   #logout
   def destroy
   end
 
+  private
+    def user_params
+      params.require(:users).permit(:username, :email, :password)
+    end
 end
