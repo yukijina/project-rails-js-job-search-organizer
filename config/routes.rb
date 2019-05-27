f286b1ee6f79d1b04c6d5c7bcd3816de63d2c0f5
yukijina <yukijina@gmail.com>
2019-05-27 10:06:32 -0700

    add route edit and update on position routes. checklist route is now nested under user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  resources :checklists
+  #resources :checklists
   #resources :positions
   resources :companies
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :companies, only: [:show] do
-    resources :positions, only: [:index, :new, :create, :show]
+    resources :positions, only: [:index, :new, :create, :show, :edit, :update]
+  end
+
+  resources :users, only: [:show] do
+    resources :checklists, only: [:index]
   end
 
   root 'sessions#home'
