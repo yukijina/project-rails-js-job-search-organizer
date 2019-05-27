289af772b045a0a2a9f0793c16b32e9c4c452ca6
yukijina <yukijina@gmail.com>
2019-05-26 20:12:33 -0700

    create new, create and position_params - not working well yet

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,3 +1,21 @@
 class PositionsController < ApplicationController
 
+  def new
+    @company = Company.find_by(id: params[:company_id])
+    @position = Position.new
+  end
+
+  def create
+    @position = Position.new(position_params)
+    binding.pry
+    if @position && @position.save
+      redirect_to company_position_path(@position.company, @position)
+    end
+  end
+
+  private
+    def position_params
+      params.require(:position).permit(:title, :description, :salary, :full_time, :comapny_id)
+    end
+
 end
