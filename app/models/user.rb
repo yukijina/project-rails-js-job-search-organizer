b8798391e46fa67c74059a2057017b6015ddef0f
yukijina <yukijina@gmail.com>
2019-05-26 20:07:14 -0700

    add signup form (new, create), show and create user_params

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_many :companies
   has_many :positions, through: :companies
   has_many :checklists
+
+  validates :username, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
