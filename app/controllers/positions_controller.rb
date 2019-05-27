6c9f418ebaa10d4eba39a2d4808a2356f052df66
yukijina <yukijina@gmail.com>
2019-05-27 09:42:56 -0700

    refactor create and add show method

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,20 +2,35 @@ class PositionsController < ApplicationController
 
   def new
     @company = Company.find_by(id: params[:company_id])
-    @position = Position.new
+    @position = @company.positions.build
+    #@position = Position.new
   end
 
   def create
-    @position = Position.new(position_params)
-    binding.pry
-    if @position && @position.save
-      redirect_to company_position_path(@position.company, @position)
+    if @company = Company.find_by(id: params[:company_id])
+      @position = @company.positions.build(position_params)
+
+      if @company && @position
+        @position.save
+        redirect_to company_position_path(@company, @position)
+      else
+        #need error message
+        render 'new'
+      end
+    #elsif !@company
+      #need error message
+      #redirect_to company_path
     end
+  end
+
+  def show
+    @position = Position.find_by(id: params[:id])
+
   end
 
   private
     def position_params
-      params.require(:position).permit(:title, :description, :salary, :full_time, :comapny_id)
+      params.require(:position).permit(:title, :description, :salary, :full_time, :company_id)
     end
 
 end
