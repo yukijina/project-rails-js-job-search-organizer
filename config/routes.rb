45d728cd1a54ae1105846de0771db814aa9d2330
yukijina <yukijina@gmail.com>
2019-05-26 20:09:31 -0700

    add nested routes (company- position)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   resources :checklists
-  resources :positions
+  #resources :positions
   resources :companies
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :companies, only: [:show] do
+    resources :positions, only: [:index, :new, :create, :show]
+  end
+
+  root 'sessions#home'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 end
