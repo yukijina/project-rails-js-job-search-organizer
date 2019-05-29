f413a8215023a107a1ab577d88f5210206bed717
yukijina <yukijina@gmail.com>
2019-05-29 06:44:48 -0700

    add logout method and update login(check password)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,22 @@ class SessionsController < ApplicationController
     render 'login'
   end
 
-  #login - create
+  #login
   def create
-    @user = User.find_by(id: params[:user][:id])
-    if @user && @user.save
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
+      #flash message
       render 'login'
     end
   end
 
   #logout
   def destroy
+    session.delete :user_id
+    redirect_to '/'
   end
 
   private
