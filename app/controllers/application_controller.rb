82bb817e1b85799c9e56f88bb6c965f098131f68
yukijina <yukijina@gmail.com>
2019-05-29 06:46:32 -0700

    add helper for logged_in? and require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?, :require_login
 
   def current_user
     User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id]
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
   end
 
 
