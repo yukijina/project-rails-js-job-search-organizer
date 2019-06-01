c0a263611b18b25f4a5c04c18c6545f3969fae8a
yukijina <yukijina@gmail.com>
2019-06-01 11:47:30 -0700

    add accepted_nested_attributes_for

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,16 @@
 class Company < ApplicationRecord
-  #has_many :users_companies
-  #has_many :users, through: :users_companies
   has_many :checklists
   has_many :users, through: :checklists
   has_many :positions
-  #accepts_nested_attributes_for :positions
+  accepts_nested_attributes_for :positions
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  def self.sort_by_name
+    Company.order(:name)
+  end
+
+  def self.sort_name_desc
+    Comapny.order(name: :desc)
+  end
 end
