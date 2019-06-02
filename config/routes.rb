4406cafb79b09a3c5a2246498eb6f08f8eb02664
yukijina <yukijina@gmail.com>
2019-06-02 15:05:02 -0700

    add destroy in checkliste route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show] do
-    resources :checklists, only: [:index, :new, :create, :show, :edit, :update]
+    resources :checklists
   end
 
   root 'sessions#home'
