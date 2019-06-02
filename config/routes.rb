a7354171eb67181c575568e68d39926afdbca360
yukijina <yukijina@gmail.com>
2019-06-02 10:49:54 -0700

    add omniauth callback route for login

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+
+#Routes for Google authentication
+  get '/auth/:provider/callback', to: 'sessions#googleAuth'
+  #get 'auth/failure', to: redirect('/')
 end
