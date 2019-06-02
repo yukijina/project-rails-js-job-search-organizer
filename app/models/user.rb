48a107af1ea03436753870e0d9ecd5f52b20ed98
yukijina <yukijina@gmail.com>
2019-06-02 10:49:03 -0700

    add link logged in through google

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
 
   validates :username, :email, :password, presence: true
   validates :email, uniqueness: true
+
+  def self.from_omniauth(auth)
+    #Creates a new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.username = auth.info.name
+      user.email = auth.info.email
+      user.password = SecureRandom.hex
+    end
+  end
 end
