0e5e19979227668e0aea5e93e2a2ea970b9071d2
yukijina <yukijina@gmail.com>
2019-06-03 22:20:10 -0700

    add helper for company_page_not_found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :logged_in?, :require_login
+  helper_method :current_user, :logged_in?, :require_login, :company_page_not_found
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -11,7 +11,13 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
+    #redirect with message
     return head(:forbidden) unless session.include? :user_id
+  end
+
+  def company_page_not_found
+    flash[:message] = "Oops. Page not found."
+    redirect_to companies_path
   end
 
 
