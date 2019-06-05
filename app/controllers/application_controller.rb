1f2029ba007f3800190bf316613447e7726106ad
yukijina <yukijina@gmail.com>
2019-06-04 23:17:14 -0700

    add admin? helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :logged_in?, :require_login, :company_page_not_found
+  helper_method :current_user, :logged_in?, :require_login, :company_page_not_found, :admin?
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
   def company_page_not_found
     flash[:message] = "Oops. Page not found."
     redirect_to companies_path
+  end
+
+  def admin?(user)
+    true if user.admin == true
   end
 
 
