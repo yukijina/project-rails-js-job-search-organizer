c9270eedb6de7a04cd2ac7bffbbf23ee3dc39069
yukijina <yukijina@gmail.com>
2019-06-04 23:18:07 -0700

    clean the code

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -3,44 +3,32 @@ class ChecklistsController < ApplicationController
 
   def index
     @user = User.find_by(id: params[:user_id])
-    if @user
-      @checklists = @user.checklists
-    else
-      page_not_found
-    end
-  end
-
-  def new
-     @user = User.find_by(id: params[:user_id])
+    @user ? @checklists = @user.checklists : page_not_found
   end
 
   def create
     company = Company.find_by(id: params[:checklist][:company_id])
     @user = User.find_by(id: params[:user_id])
     @checklist = Checklist.find_position(params)
+
     if @checklist.persisted?
       flash[:message] = "This position is already in your checklist."
       redirect_back fallback_location: company_positions_path(company)
     else
-      @checklist && @checklist.save
+      @checklist.save
       redirect_to user_checklists_path(@user)
     end
-  end
-
-  def show
-    page_not_found
   end
 
   def edit
     @user = User.find_by(id: params[:user_id])
     @checklist = Checklist.find_by(id: params[:id])
-
   end
 
   def update
     if params[:user_id]
-      @checklist = Checklist.find_by(id: params[:id])
-      @checklist.update(checklist_params)
+      checklist = Checklist.find_by(id: params[:id])
+      checklist.update(checklist_params)
       redirect_to user_checklists_path(current_user)
     end
   end
@@ -62,6 +50,3 @@ class ChecklistsController < ApplicationController
     end
 
 end
-
-
-#Checklist.where(user_id: params[:user_id], position_id: params[:checklist][:position_id]).first_or_initialize {|list| list.company_id = params[:checklist][:company_id]}
