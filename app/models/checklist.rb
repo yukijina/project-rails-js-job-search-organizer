6e0e58aaaa9e9fc496908b7633c8f10e7458aab9
yukijina <yukijina@gmail.com>
2019-06-04 22:04:22 -0700

    add find_position(params). Create only if a user does not have the position

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -7,5 +7,15 @@ class Checklist < ApplicationRecord
     order(resume: :desc)
   end
 
+  def self.find_position(params)
+    where(["user_id = ? AND position_id = ?", params[:user_id], params[:checklist][:position_id]]).first_or_initialize  do |checklist|
+      checklist.user_id = params[:user_id]
+      checklist.position_id = params[:checklist][:position_id]
+      checklist.company_id = params[:checklist][:company_id]
+    end
+  end
+
+
+
 
 end
