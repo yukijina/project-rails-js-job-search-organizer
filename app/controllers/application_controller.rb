ae45bf348e6d51d28a7f328abb00c63a6006f631
yukijina <yukijina@gmail.com>
2019-06-06 21:43:42 -0700

    move admin? to application_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :logged_in?, :require_login, :company_page_not_found, :admin?
+  helper_method :current_user, :logged_in?, :require_login, :company_page_not_found
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -18,10 +18,5 @@ class ApplicationController < ActionController::Base
     flash[:message] = "Oops. Page not found."
     redirect_to companies_path
   end
-
-  def admin?(user)
-    true if user.admin == true
-  end
-
 
 end
