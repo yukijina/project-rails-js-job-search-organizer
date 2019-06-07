13dea3955e1932c9f5aaaef28ae08060b454d8b3
yukijina <yukijina@gmail.com>
2019-06-07 06:45:41 -0700

    refactor admin sign up (create)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,33 +8,34 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if params[:user][:admin] == "1"
-      if params[:user][:admin_code] == "007"
-        @user.save
+    if admin_params || non_admin_params
+      if @user && @user.save
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
-        flash[:message] = "Admin code is not correct. Please try again or signup as non-admin."
-       render 'new'
+        render 'new'
       end
-    elsif params[:user][:admin] == "0"
-        @user.admin_code = nil
-        @user && @user.save
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-    else
+    elsif !admin_params && !non_admin_params
+      flash[:message] = "Admin code is not correct."
       render 'new'
     end
   end
 
   def show
     @user = User.find_by(id: params[:id])
-
   end
 
   private
     def user_params
       params.require(:user).permit(:username, :email, :password, :admin, :admin_code)
+    end
+
+    def admin_params
+      params[:user][:admin] == "1" && params[:user][:admin_code] == "007"
+    end
+
+    def non_admin_params
+      params[:user][:admin] == "0"
     end
 
 end
