3e80beb932371cbd9c04f4833b481eb73672816d
yukijina <yukijina@gmail.com>
2019-08-02 19:51:26 -0700

    bug fix for clickReadMore()

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,9 +14,9 @@ class Company {
     return `
     <div class="card-body">
     <h2 class="card-title">${this.name}</h2>
-    <p class="card-text">URL: ${this.url}</p>
-    <p class="card-text" id="js-description">${this.description.substring(0, 20)}...</p>
-    <button class="js-read-more btn btn-info btn-sm" > Read More </button>
+    <a href="${this.url}" class="card-text" target="_blank"><i class="fas fa-globe"></i> Website</a>
+    <p class="card-text js-description-${this.id}">${this.description.substring(0, 20)}...</p>
+    <button class="js-read-more btn btn-info btn-sm" data-id="${this.id}"> Read More </button>
     </div>
     `
   }
@@ -35,13 +35,13 @@ function listeningCompaniesLoad() {
 }
 
 function clickReadMore() {
-  const descriptionDiv = document.getElementById("js-description");
-  document.querySelector(".js-read-more").addEventListener("click", function(e) {
+  $(".js-read-more").on("click", function(e) {
     e.preventDefault();
-    $.get("/companies" + ".json", function(res) {
-      res.forEach(function(companyData) {
-        descriptionDiv.innerText = new Company(companyData).description
-      })
+    const id = this.dataset.id;
+    $.get("/companies/" + id + ".json", function(res) {
+      const company = new Company(res)
+      const descriptionDiv = document.querySelector(`.js-description-${company.id}`);
+      descriptionDiv.innerText = company.description
     })
   })
 }
