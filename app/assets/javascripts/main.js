67f89f8a5f1e04219cef91e7772d925259e69eaf
yukijina <yukijina@gmail.com>
2019-08-03 07:25:38 -0700

    created positionDetails function. Display position details when btn clicked

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,10 @@
 $(function() {
   console.log("Loading..!")
-  listeningCompaniesLoad();
+  if (window.location.pathname === "/companies") {
+    listeningCompaniesLoad()
+  } else if (/\d/.test(window.location.pathname)) {
+    displayCompanyShow();
+  }
 })
 
 class Company {
@@ -17,14 +21,50 @@ class Company {
     <a href="${this.url}" class="card-text" target="_blank"><i class="fas fa-globe"></i> Website</a>
     <p class="card-text js-description-${this.id}">${this.description.substring(0, 20)}...</p>
     <button class="js-read-more btn btn-info btn-sm" data-id="${this.id}"> Read More </button>
+    <a href="/companies/${this.id}" class="js-positions btn btn-info btn-sm" data-id="${this.id}">Check Positions</a>
     </div>
     `
   }
 }
+// <a href="/companies/${this.id}" class="js-positions btn btn-info btn-sm" data-id="${this.id}">Check Positions</a>
+
+class Position {
+  constructor(data) {
+    this.id = data.id;
+    this.title = data.title;
+    this.description = data.description;
+    this.salary = data.salary;
+    this.fullTime = data.full_time;
+    this.created = data.created_at;
+    this.updated = data.updated_at;
+    this.companyId = data.company_id;
+  }
+  positionFormatHTML() {
+    return `
+      <div id="position-wrapper-${this.id}">
+        <h2>${this.title}</h2>
+        <p>${this.description.substring(0, 5)}...</p>
+        <p>Salary: ${this.salary} a year</p>
+        <button class="js-position btn btn-info btn-sm" data-id="${this.id}" data-companyid="${this.companyId}">More info</button>
+      </div>
+    `
+  }
+  positionDetailsHTML() {
+    return `
+      <div>
+      <h2>${this.title}</h2>
+      <p>Description: ${this.description}</p>
+      <p>Full Time: ${this.fullTime}</p>
+      <p>Position Created: ${this.created}</p>
+      <p>Updated: ${this.updated}</p>
+      </div>
+    `
+  }
+}
+
 
 function listeningCompaniesLoad() {
   $.get("/companies" + ".json", function(res) {
-    console.log(res)
     const wrapper = document.getElementById("companies-wrapper");
     res.forEach(function(companyData) {
       const company = new Company(companyData)
@@ -43,5 +83,36 @@ function clickReadMore() {
       const descriptionDiv = document.querySelector(`.js-description-${company.id}`);
       descriptionDiv.innerText = company.description
     })
+  })
+}
+
+// Company Show Page
+function displayCompanyShow() {
+    console.log("Now show")
+    let positionsWrapper = document.getElementById("positions-wrapper")
+    const id = positionsWrapper.dataset.id;
+    $.get("/companies/" + id + ".json", function(res) {
+      const company = new Company(res)
+      res.positions.forEach(function(positionData) {
+        let position = new Position(positionData)
+        positionsWrapper.innerHTML += position.positionFormatHTML();
+        positionDetails();
+      })
+  })
+}
+
+
+function positionDetails() {
+  $(".js-position").on("click", function(e) {
+    e.preventDefault();
+    console.log("clicked!")
+    const positionId = this.dataset.id;
+    const companyId = this.dataset.companyid;
+    $.get("/companies/" + companyId + "/positions/" + positionId + ".json", function(res) {
+      console.log(res)
+        const position = new Position(res)
+        const positionDiv = document.getElementById(`position-wrapper-${positionId}`)
+        positionDiv.innerHTML = position.positionDetailsHTML();
+      })
   })
 }
