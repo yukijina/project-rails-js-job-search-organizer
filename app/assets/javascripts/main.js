040dea236e07ca5cd8e35bec44f225e73aeddc64
yukijina <yukijina@gmail.com>
2019-08-02 17:49:47 -0700

    create clickReadMore function. Display whole description when btn is clicked

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 $(function() {
   console.log("Loading..!")
-  listningCompaniesLoad();
+  listeningCompaniesLoad();
 })
 
 class Company {
@@ -15,22 +15,33 @@ class Company {
     <div class="card-body">
     <h2 class="card-title">${this.name}</h2>
     <p class="card-text">URL: ${this.url}</p>
-    <p class="card-text">${this.description.substring(0, 20)}...</p>
-    <button class="js-read-more btn btn-info btn-sm" data-id="${this.id}"> Read More </button>
+    <p class="card-text" id="js-description">${this.description.substring(0, 20)}...</p>
+    <button class="js-read-more btn btn-info btn-sm" > Read More </button>
     </div>
     `
   }
 }
 
-function listningCompaniesLoad() {
+function listeningCompaniesLoad() {
   $.get("/companies" + ".json", function(res) {
     console.log(res)
     const wrapper = document.getElementById("companies-wrapper");
     res.forEach(function(companyData) {
       const company = new Company(companyData)
       wrapper.innerHTML += company.indexHTML();
+      clickReadMore();
     })
   })
+}
 
-
+function clickReadMore() {
+  const descriptionDiv = document.getElementById("js-description");
+  document.querySelector(".js-read-more").addEventListener("click", function(e) {
+    e.preventDefault();
+    $.get("/companies" + ".json", function(res) {
+      res.forEach(function(companyData) {
+        descriptionDiv.innerText = new Company(companyData).description
+      })
+    })
+  })
 }
