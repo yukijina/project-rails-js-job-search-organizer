4ab3c70eda71a03a1adbccee2baa8f965aca54f1
yukijina <yukijina@gmail.com>
2019-08-04 16:52:46 -0700

    organize company routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :companies
+  resources :companies, except: [:new, :create]
   resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :companies, only: [:show] do
