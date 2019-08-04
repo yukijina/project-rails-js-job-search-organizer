8da6e077737ee69d5cad406407089f7565d0f21c
yukijina <yukijina@gmail.com>
2019-08-03 23:36:10 -0700

    add attribute

--- app/serializers/position_serializer.rb
+++ app/serializers/position_serializer.rb
@@ -1,4 +1,11 @@
 class PositionSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :salary, :full_time, :created_at, :updated_at, :company_id
   belongs_to :company
+
+  # def company
+  #   binding.pry
+  #    id = self.object.company_id
+  #    { company_id: id,
+  #      name: Company.find_by(id: id).name  }
+   #end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
   has_many :checklists
+  has_many :companies
+  has_many :positions
+
 end
