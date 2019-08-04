08c89dcfce26d922bbf72baa226c56503085db97
yukijina <yukijina@gmail.com>
2019-08-04 10:59:20 -0700

    add helper for position full time and saraly column

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,8 +12,25 @@ module ApplicationHelper
     end
   end
 
+  def display_job_type(text)
+    if text == false
+      "Non-Full Time Position"
+    else
+      "Full Time"
+    end
+  end
+
   def display_time(time)
     time.strftime("%m/%d/%y")
+  end
+
+  def display_salary(salary)
+    if salary == 0
+      "n/a"
+    else
+      number_to_currency(salary, precision: 0) + " / year"
+      #presidion(decimal)
+    end
   end
 
 
