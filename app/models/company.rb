c9e56d84b89de3c3883d10de6efe1067b388f268
yukijina <yukijina@gmail.com>
2019-08-04 14:54:22 -0700

    update nested_attribute

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ApplicationRecord
   has_many :checklists
   has_many :users, through: :checklists
   has_many :positions
-  accepts_nested_attributes_for :positions
+  #accepts_nested_attributes_for :positions
 
   validates :name, :url, presence: true, uniqueness: true
 
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,7 @@
 class Position < ApplicationRecord
   belongs_to :company
   has_many :checklists
+  accepts_nested_attributes_for :company
 
   validates :title, :description, :salary, presence: true
 
