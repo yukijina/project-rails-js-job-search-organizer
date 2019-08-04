1ffa27b1fe5c75c7e9d03d06133b16ae5b3436f7
yukijina <yukijina@gmail.com>
2019-08-03 23:35:45 -0700

    show position in companies page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,9 +3,13 @@ $(function() {
   if (window.location.pathname === "/companies") {
     listeningCompaniesLoad()
   } else if (window.location.pathname.includes("/users")) {
-    displayChecklist();
-  } else if (window.location.pathname.includes("/companies") && /\d/.test(window.location.pathname)) {
-    displayCompanyShow();
+    clickChecklistBtn();
+  } else if (window.location.pathname.includes("/companies") && /\d/.test(window.location.pathname) && !window.location.pathname.includes("/positions")) {
+    //displayCompanyShow();
+  } else if (window.location.pathname.includes("/companies") && /\d/.test(window.location.pathname) && window.location.pathname.includes("/positions")) {
+    clickToPosition();
+  } else if (window.location.pathname.includes("/companies") && /\d/.test(window.location.pathname) && window.location.pathname.includes("/positions") && /\d/.test(window.location.pathname)) {
+    displayPositionShow()
   }
 })
 
@@ -23,7 +27,9 @@ class Company {
     <a href="${this.url}" class="card-text" target="_blank"><i class="fas fa-globe"></i> Website</a>
     <p class="card-text js-description-${this.id}">${this.description.substring(0, 20)}...</p>
     <button class="js-read-more btn btn-info btn-sm" data-id="${this.id}"> Read More </button>
-    <a href="/companies/${this.id}" class="js-positions btn btn-info btn-sm" data-id="${this.id}">Check Positions</a>
+    <button class="js-positions btn btn-info btn-sm" data-id="${this.id}">Check Positions</button>
+    <hr style="margin: 30px;">
+    <div class="append-positions-${this.id}" style="display:none;"></div>
     </div>
     `
   }
@@ -45,21 +51,22 @@ class Position {
     return `
       <div id="position-wrapper-${this.id}">
         <h2>${this.title}</h2>
-        <p>${this.description.substring(0, 5)}...</p>
+        <p>${this.description.substring(0, 20)}...</p>
         <p>Salary: ${this.salary} a year</p>
         <button class="js-position btn btn-info btn-sm" data-id="${this.id}" data-companyid="${this.companyId}">More info</button>
       </div>
     `
   }
+
+  //href="/companies/${this.companyId}/positions/${this.id}"
   positionDetailsHTML() {
     return `
-      <div>
       <h2>${this.title}</h2>
       <p>Description: ${this.description}</p>
       <p>Full Time: ${this.fullTime}</p>
       <p>Position Created: ${this.created}</p>
       <p>Updated: ${this.updated}</p>
-      </div>
+      <a href="/companies/${this.companyId}/positions" class="js-add-checklist btn btn-outline-info btn-sm" data-id="${this.id}" data-companyid="${this.companyId}">Go to List</a>
     `
   }
 }
@@ -72,6 +79,7 @@ function listeningCompaniesLoad() {
       const company = new Company(companyData)
       wrapper.innerHTML += company.indexHTML();
       clickReadMore();
+      displayPositions();
     })
   })
 }
@@ -88,22 +96,28 @@ function clickReadMore() {
   })
 }
 
-// Company Show Page
-function displayCompanyShow() {
-    console.log("Now show")
-    let positionsWrapper = document.getElementById("positions-wrapper")
-    const id = positionsWrapper.dataset.id;
-    $.get("/companies/" + id + ".json", function(res) {
-      const company = new Company(res)
-      res.positions.forEach(function(positionData) {
-        let position = new Position(positionData)
-        positionsWrapper.innerHTML += position.positionFormatHTML();
-        positionDetails();
-      })
+//Display positions in index page
+function displayPositions() {
+  $(".js-positions").on("click", function(e) {
+    e.preventDefault();
+    const companyId = this.dataset.id;
+    $.get("/companies/" + companyId + ".json", function(res) {
+      let positions = res.positions.map(function(position) {
+        return new Position(position).positionFormatHTML()
+      }).join("")
+      const div = document.querySelector(`.append-positions-${companyId}`)
+      div.innerHTML = positions
+      if (div.style.display === "none") {
+        div.style.display = "block"
+      } else {
+        div.style.display = "none"
+      }
+      positionDetails()
+    })
   })
 }
 
-
+// Display position Details
 function positionDetails() {
   $(".js-position").on("click", function(e) {
     e.preventDefault();
@@ -116,5 +130,35 @@ function positionDetails() {
         const positionDiv = document.getElementById(`position-wrapper-${positionId}`)
         positionDiv.innerHTML = position.positionDetailsHTML();
       })
+  })
+}
+
+// Company Show Page
+// function displayCompanyShow() {
+//     console.log("Now show")
+//     let positionsWrapper = document.getElementById("positions-wrapper")
+//     const id = positionsWrapper.dataset.id;
+//     $.get("/companies/" + id + ".json", function(res) {
+//       const company = new Company(res)
+//       res.positions.forEach(function(positionData) {
+//         let position = new Position(positionData)
+//         positionsWrapper.innerHTML += position.positionFormatHTML();
+//         positionDetails();
+//       })
+//   })
+// }
+
+
+
+
+// Not sure where
+function clickToPosition() {
+  $(".js-to-position").on("click", function(e) {
+    e.preventDefault();
+    const positionId = this.dataset.positionid;
+    const companyId = this.dataset.companyid;
+    $.get("/companies/" + companyId + "/positions/" + positionId + ".json", function(res) {
+      window.location.replace(`/companies/${companyId}/positions/${positionId}`);
+    })
   })
 }
