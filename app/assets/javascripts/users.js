ceae03963f33209e2dc8b55e61f5d98432448f24
yukijina <yukijina@gmail.com>
2019-08-04 19:00:18 -0700

    add user class - may not use and delete later

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,17 @@
-function displayChecklist() {
-  $("#js-display-checklist").on("click", function(e) {
-    e.preventDefault();
-    
-  })
+class Checklist {
+  constructor(data) {
+    this.id = data.id;
+    this.resume = data.resume;
+    this.interview = data.interview;
+    this.complete = data.complete;
+    this.note = data.note;
+    this.userId = data.user_id;
+    this.companyId = data.company_id;
+    this.positionId = data.position_id;
+  }
+  formatChecklist() {
+    return `
+
+    `
+  }
 }
