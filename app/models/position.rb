1258653b6849904fa66ca7890af05086868059b7
yukijina <yukijina@gmail.com>
2019-08-09 06:52:46 -0700

    add users association

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,7 +2,7 @@ class Position < ApplicationRecord
   belongs_to :company
   has_many :checklists
   accepts_nested_attributes_for :company
-
+  has_many :users, through: :checklists
   validates :title, :description, :salary, presence: true
 
   def self.sort_by_title
